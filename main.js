9c17f3b99000ffaf6b0deb44443ad8c5887057ee
Xutrurn <kanjoskillz@gmail.com>
2021-02-09 14:49:31 +0100

    caricamento dati in html via api completato

---
+++ main.js
@@ -0,0 +1,35 @@
+// alert("Hello world");
+
+// Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali.
+//
+// Stampiamo tutto a schermo, in questo momento non è importante la parte grafica.
+//
+// Bonus:
+// Creare una select con i seguenti generi: pop, rock, metal e jazz.
+// In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+//
+// Chiamata: https://flynn.boolean.careers/exercises/api/array/music
+
+
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    dischi: [],
+    // tipi: ['All']
+  },
+  mounted() {
+
+    axios
+      .get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then((result) => {
+        // console.log(result.data.response);
+        this.dischi = result.data.response;
+      })
+      .catch(error => console.log('errore', error));
+  }
+
+
+});
+
+// console.log();
